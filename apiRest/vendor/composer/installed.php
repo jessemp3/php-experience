a36ae4ef655d613dc323dc12a21984ba88d778d0
jesse <nitchsx@gmail.com>
2024-12-21 22:22:01 -0300

    feat: class of API Rest

---
+++ apiRest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jesse/api-rest',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c3993cddf5e5c670bac76ffa8c9a44517e8cce92',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jesse/api-rest' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c3993cddf5e5c670bac76ffa8c9a44517e8cce92',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
